7348c3bf4738ed053b3867e58615e1dc0381e325
shady-mungai <shadrackmungai10@gmail.com>
2022-11-24 15:48:39 +0300

    Completed Assignment on Functions lab

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Code your solution in this file!
+const blocksFromHq = 42;
+let num1;
+let num2;
+let amountPayable;
+
+// Distance in blocks
+function distanceFromHqInBlocks(valueInBlocks) {
+  num1 = Math.abs(valueInBlocks - blocksFromHq);
+  return num1;
+}
+distanceFromHqInBlocks(45);
+
+// calculating distance in feet
+function distanceFromHqInFeet(valueInBlocks) {
+  distanceFromHqInBlocks(valueInBlocks);
+  num2 = num1 * 264;
+  return num2;
+}
+distanceFromHqInFeet(45);
+// calculating Distance travelled
+function distanceTravelledInFeet(start, destination) {
+  let num3 = Math.abs(destination - start) * 264;
+  return num3;
+}
+
+distanceTravelledInFeet(42, 45);
+
+// Calculating Fare price
+
+function calculatesFarePrice(start, destination)
+ {let num4= Math.abs(((destination - start)*264));
+
+
+
+  if (num4 <= 400) {
+    return 0;
+  } else 
+        if (num4 > 400 && num4 < 2000) {
+     amountPayable = ((num4 - 400) * 0.02);
+    return amountPayable;
+  } 
+  else 
+        if(num4>=2000 && num4<2500){
+    return 25;
+  }
+  else 
+        if (num4 > 2500) {
+    return 'cannot travel that far';
+  }
+}
+calculatesFarePrice();
+
+
